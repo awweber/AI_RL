41cc32015fdf95227dbc3027f6fb542e0a8c08b2
Alexander Weber <awweber197@gmail.com>
2025-10-16 09:07:56 +0200

    Abschnitt 3: Grundlagen zu RL

--- Chapter03/agentMDP.py
+++ Chapter03/agentMDP.py
@@ -1,15 +1,21 @@
+# Kapitel 3: Grundlagen zu Reinforcement Learning (RL)
+# Zustände, Aktionen und Belohnungen
+# Markov-Entscheidungsprozess (MDP)
 STATES = ["a", "b"]
 ACTIONS = ["a", "b"]
 REWARDS = {"a": {"a": 0, "b": 7}, "b": {"a": -5, "b": 0}}
 
-
+# Markov-Entscheidungsprozess (MDP)
 def main() -> None:
+    # Initialisierung
     state = "a"
     reward = 0
     total_reward = 0
 
+    # Ausgabe des Startzustands und der Startbelohnung
     print(f"Start-State: {state} Start-Reward: {reward}\n\n")
 
+    # Simuliere 10 Iterationen
     for i in range(1, 11):
         print(f"State: {state} - Iteration: {i}")
         action = input("Action: ")
@@ -23,6 +29,6 @@ def main() -> None:
                 f"Total-Reward: {total_reward}",
             )
 
-
+# Starte das Programm
 if __name__ == "__main__":
     main()
